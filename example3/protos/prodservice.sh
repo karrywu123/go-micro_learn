e5aa99b45786dd15c748c3c984f12a45a41fff04
karrywu <38031335+karrywu123@users.noreply.github.com>
2021-07-23 10:12:34 +0700

    Create prodservice.sh

---
+++ example3/protos/prodservice.sh
@@ -0,0 +1,20 @@
+go get -u github.com/asim/go-micro/v3
+
+go get -u github.com/asim/go-micro/plugins/registry/consul/v3
+
+go get -u github.com/golang/protobuf/protoc-gen-go
+
+go get -u github.com/micro/micro/v3/cmd/protoc-gen-micro
+
+
+protoc -I . --go_out=plugins=grpc:. ./Models.proto
+
+protoc -I . --go_out=plugins=grpc:. ./ProdService.proto
+
+
+protoc --micro_out=. --go_out=. ./Models.proto
+
+protoc --micro_out=. --go_out=. ./ProdService.proto
+
+protoc-go-inject-tag -input=. ./services/Models.pb.go
+protoc-go-inject-tag -input=. ./services/ProdService.pb.go
